211366a57854c983ce5a8beb856c968a894829c3
MichaelVanatta <michaelvanatta12@gmail.com>
2025-04-01 16:05:07 -0600

    C++ intro

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	cout << "Hello, World!" << endl;
-	cout << "Git is now tracking this project!" << endl;
-	return 0;
+int main() 
+{
+	cout << "Hello, World\n!";
+	cout << "Git is now tracking this project!\n";
+	
+	int age;
+	cout << "Enter age: " << endl;
+	cin >> age;
+
+	cout << "Your age is " << age << endl;
 }
