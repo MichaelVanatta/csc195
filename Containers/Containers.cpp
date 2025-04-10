1086b581d70a0def1030bdf063fc253084ff6eb5
MichaelVanatta <michaelvanatta12@gmail.com>
2025-04-10 16:16:03 -0600

    Daily commit and push

--- Containers/Containers.cpp
+++ Containers/Containers.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <list>
+#include <map>
 using namespace std;
 
 int main()
@@ -20,4 +22,53 @@ int main()
         cout << numbers[i] << endl;
     }
 
+    cout << "-------------------------------------" << endl;
+
+    // Pointer Arithmetic
+    string chemicals[5] = { "mercury", "hydrochloric acid", "chlorine", "bromine", "salt" };
+
+    cout << chemicals[2] << endl;
+    cout << chemicals << endl; //returns address of first element in the array
+    cout << &chemicals[0] << endl;
+
+    cout << "--------------------------------------" << endl;
+
+    for (int i = 0; i < chemicals->size(); i++) {
+        cout << &chemicals[i] << endl;
+    }
+
+    cout << "--------------------------------------" << endl;
+
+    string* p = chemicals;
+    cout << p << endl;
+    cout << ++p << endl;
+    cout << p++ << endl;
+    cout << p + 2 << endl;
+    cout << p - 1 << endl;
+
+    cout << "--------------------------------------" << endl;
+
+    // containers
+    list<string> cars = { "Hyundai", "Ferrari", "Mercedes", "Volvo" };
+
+    cout << cars.front() << endl;
+    cout << cars.back() << endl;
+
+    cars.pop_back();
+    cout << cars.back() << endl;
+
+    cars.push_front("Tesla");
+    cout << cars.front() << endl;
+
+    cout << "--------------------------------------" << endl;
+
+    map<string, int> pet = { {"Dogs", 23}, {"Chameleons", 3}, {"Turtle", 43} };
+
+    pet["Dogs"] = 50;
+
+    cout << pet["Dogs"] << endl;
+
+    pet["Spiders"] = 2;
+
+    cout << pet.empty() << endl; // 0 means has elements while 1 means empty
 }
