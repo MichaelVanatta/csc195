010e75f90fbfe801b5cb0f07848710a76e519594
MichaelVanatta <michaelvanatta12@gmail.com>
2025-04-23 16:18:00 -0600

    Daily Commit

---
+++ Operator/Main.cpp
@@ -0,0 +1,24 @@
+#include "Point.h"
+using namespace std;
+using namespace ost;
+
+// operator overloads outside a class requires the right and left sides of the symbol as parameters. Left side is the instance that we are in
+//Point& operator + (Point& p1, Point& p2) 
+//{
+//	Point point;
+//	point.x = p1.x += p2.x;
+//	point.y = p1.y += p2.y;
+//	return point;
+//}
+
+int main()
+{
+	Point point1(10,14);
+	point1.Write(cout);
+
+	Point point2(5,8);
+	//point1.Add(point2);	---   Instead of a method call
+	Point point3 = (point1 + point2) * 3.0f;		// syntactical sugar(looks better), gives operators user-defined meaning
+
+	point3.Write(cout);
+}
---
+++ Operator/Point.cpp
@@ -0,0 +1,29 @@
+#include "Point.h"
+
+namespace ost
+{
+	void Point::Write(ostream& ostream)
+	{
+		ostream << x << " : " << y << std::endl;
+	}
+
+	void Point::Add(Point& point)
+	{
+		x += point.x;
+		y += point.y;
+	}
+
+	// operator overloads inside a class just requires the right side of the symbol as a parameter
+	Point& Point::operator+(Point& point)
+	{
+		Point p;
+		p.x = x + point.x;
+		p.y = y + point.y;
+		return p;
+	}
+
+	Point Point::operator*(float s)
+	{
+		return Point(x * s, y * s);
+	}
+}
